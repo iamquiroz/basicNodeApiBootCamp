16bd010f19d405fe716c77a8f6c87ae714f9002f
jquirozgerardo <jquiroz.mexicali@gmail.com>
2020-10-23 01:49:17 -0700

    database conn

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = () => {
+    const conn =   mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    });
+
+    console.log(`MongoDB Connected: `);
+}
+
+module.exports = connectDB;
