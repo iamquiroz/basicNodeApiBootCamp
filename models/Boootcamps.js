7eda636e6a5fe09b176fd0c715e98573d638f898
jquirozgerardo <jquiroz.mexicali@gmail.com>
2020-10-23 13:30:26 -0700

    add model bootcamps.js

---
+++ models/Boootcamps.js
@@ -0,0 +1,103 @@
+const mongoose =  require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [ true, 'Please add a name'],
+        unique: true,
+        trim: true,
+        maxlenght: [50, 'Name can not be more than 50 characters']
+    },
+    slug: String,
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        maxlenght: [500, 'Description can not be more than 500 characters']
+    },
+    website: {
+        type: String,
+        match: [
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+                'Please use a valid URL with HTTP or HTTPS'
+        ]
+    },
+    phone: {
+        type: String,
+        maxlenght: [20, 'Phone number can not be longer than 20 characters']
+    },
+    email: {
+        type: String,
+        match: [
+            /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+            'Please add a valid email'
+        ]
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+    },
+    location: {
+        // GeoJson Point
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            require: true,
+            index: '2dsphere'
+        },
+        formattedAddress: String,
+        string: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String
+    },
+    careers: {
+        // Array of strings
+        type: [String],
+        required: true,
+        enum: [
+            'Web Development',
+            'Mobile Development',
+            'UI/UX',
+            'Data Science',
+            'Business',
+            'Other'
+        ]
+    },
+    averageRating: {
+        type: Number,
+        min: [1, 'Rating must be at least 1'],
+        max: [10, 'Rating must can not be more than 10']
+    },
+    averageCost: Number,
+    photo: {
+        type: String,
+        default: 'no-photo.jpg'
+    },
+    housing: {
+        type: Boolean,
+        default: false
+    },
+    jobAssistance: {
+        type: Boolean,
+        default: false
+    },
+    jobGuarantee: {
+        type: Boolean,
+        default: false
+    },
+    acceptGi: {
+        type: Boolean,
+        default: false
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
